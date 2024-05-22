acf5a20c8668770bcd21dfe0464b2d711515355c
djoleluk <djoleluk@gmai.com>
2024-05-22 18:17:56 +0200

    Created a static server for servind React app

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000; // or any port you prefer
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle API routes
+app.get('/api/message', (req, res) => {
+    console.log("GET request received!");
+    const currentTime = new Intl.DateTimeFormat('en-US', {
+        timeZone: 'Europe/Belgrade',
+        dateStyle: 'full',
+        timeStyle: 'long'
+    }).format(new Date());
+    res.json({ message: `Current time is: ${currentTime}` });
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
