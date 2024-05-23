f47f764dec39e6efa42781e411fbea75e60f84f2
djoleluk <djoleluk@gmai.com>
2024-05-23 13:19:37 +0200

    new commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 // API endpoint to get the current time
 app.get('/api/message', (req, res) => {
@@ -28,19 +28,20 @@ app.get('/api/message', (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
 });
 
 // Read SSL certificate and key
 const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
+    key: fs.readFileSync(path.join(__dirname, '..', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '..', 'cert.pem'))
 };
 
 // Start the HTTPS server
 https.createServer(options, app).listen(port, '0.0.0.0', () => {
     console.log(`Server running at https://0.0.0.0:${port}/`);
 });
+
 
 
 
