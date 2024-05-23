1383845f444032ef05673e4d3a78d58fcd178cc4
djoleluk <djoleluk@gmai.com>
2024-05-23 12:32:08 +0200

    File server.js updated to hangle https protocol

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Enable CORS for your React app
 app.use(cors({
-  origin: 'http://13.48.26.177:3001'
+    origin: 'https://13.48.26.177:3001'
 }));
 
 // Serve the static files from the React app
@@ -15,23 +17,31 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // API endpoint to get the current time
 app.get('/api/message', (req, res) => {
-  console.log("GET request received!");
-  const currentTime = new Intl.DateTimeFormat('en-US', {
-    timeZone: 'Europe/Belgrade',
-    dateStyle: 'full',
-    timeStyle: 'long'
-  }).format(new Date());
-  res.json({ message: `Datum i vreme: ${currentTime}` });
+    console.log("GET request received!");
+    const currentTime = new Intl.DateTimeFormat('en-US', {
+        timeZone: 'Europe/Belgrade',
+        dateStyle: 'full',
+        timeStyle: 'long'
+    }).format(new Date());
+    res.json({ message: `Datum i vreme: ${currentTime}` });
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}/`);
+// Read SSL certificate and key
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+// Start the HTTPS server
+https.createServer(options, app).listen(port, '0.0.0.0', () => {
+    console.log(`Server running at https://0.0.0.0:${port}/`);
 });
+
 
 
 
