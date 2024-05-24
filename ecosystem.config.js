119897b976685a664eda46502faa0d3fa5a70565
djoleluk <djoleluk@gmai.com>
2024-05-24 19:49:56 +0200

    New commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'ngrok',
       args: 'http 3001',
       env: {
-        NGROK_AUTHTOKEN: '<your-ngrok-authtoken>'
+        NGROK_AUTHTOKEN: '1dARQsaFZoUv7VOAuMk5BZqdel9_6Ro9GVuFaxXd9Z6dkr719'
       }
     }
   ]
