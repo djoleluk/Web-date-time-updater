c100ed55f99b5ea723444158b5562793e64a00d8
djoleluk <djoleluk@gmai.com>
2024-05-25 19:41:38 +0200

    New commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3001;
+const path = require('path');
 
 // Enable CORS for your React app
 app.use(cors({
