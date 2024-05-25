0c253a22e446cfc82b8719dc6a719b924769da8a
djoleluk <djoleluk@gmai.com>
2024-05-25 09:58:03 +0200

    New commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,22 @@ module.exports = {
       script: 'ngrok',
       args: 'http 3001',
       env: {
-        NGROK_AUTHTOKEN: '1dARQsaFZoUv7VOAuMk5BZqdel9_6Ro9GVuFaxXd9Z6dkr719' 
+        NGROK_AUTHTOKEN: '1dARQsaFZoUv7VOAuMk5BZqdel9_6Ro9GVuFaxXd9Z6dkr719' // Replace with your actual ngrok authtoken
       }
+    },
+    {
+      name: 'date-time-updater-service',
+      script: './server/server.js', // Adjust this according to the location of your server script
+      watch: true, // Optional: restart the service on changes
+    },
+    {
+      name: 'react-app-service',
+      script: 'npm',
+      args: 'start',
+      cwd: './client', // Change to the client directory to run the React app
+      watch: true, // Optional: restart the service on changes
     }
   ]
 };
+
 
