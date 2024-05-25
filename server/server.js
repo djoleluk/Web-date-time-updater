8352a87390f98bf9804997a7dd6661b103809d33
djoleluk <djoleluk@gmai.com>
2024-05-25 10:14:17 +0200

    New commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = 3001;
 
 // Enable CORS for all origins during development
 app.use(cors({
-  origin: 'https://13.48.26.177:3001'
+    origin: 'http://localhost:3000'
 }));
 
 // Serve the static files from the React app
